300eaa28972b94a58a2fc571d9ef5f051aae84f0
benjaminrall <33876244+benjaminrall@users.noreply.github.com>
2019-09-10 11:58:53 +0100

    Update level_editor.py

--- level_editor.py
+++ level_editor.py
@@ -68,58 +68,58 @@ road_e_l = pygame.image.load("tiles\\road_end_left.png")
 road_e_r = pygame.image.load("tiles\\road_end_right.png")
 
 images =(
-(0, dirt_1),
-(1, dirt_2),
-(2, grass_1),
-(3, grass_2),
-(4, c_junction),
-(5, dirt_b_s_b_l),
-(6, dirt_b_s_b_r),
-(7, dirt_b_c_d),
-(8, dirt_b_c_b_l),
-(9, dirt_b_c_b_r),
-(10, dirt_b_c_l),
-(11, dirt_b_c_r),
-(12, dirt_b_c_u),
-(13, dirt_b_c_t_l),
-(14, dirt_b_c_t_r),
-(15, dirt_b_s_d),
-(16, dirt_b_s_l),
-(17, dirt_b_s_r),
-(18, dirt_b_s_u),
-(19, dirt_b_s_t_l),
-(20, dirt_b_s_t_r),
-(21, grass_b_s_b_l),
-(22, grass_b_s_b_r),
-(23, grass_b_s_t_l),
-(24, grass_b_s_t_r),
-(25, grass_b),
-(26, l_junction_b_l),
-(27, l_junction_b_r),
-(28, l_junction_t_l),
-(29, l_junction_t_r),
-(30, road_e_d),
-(31, road_e_l),
-(32, road_e_r),
-(33, road_e_u),
-(34, road_h),
-(35, road_t_dirt_d),
-(36, road_t_dirt_l),
-(37, road_t_dirt_r),
-(38, road_t_dirt_u),
-(39, road_t_c_b_l),
-(40, road_t_c_b_r),
-(41, road_t_c_t_l),
-(42, road_t_c_t_r),
-(43, road_t_s_b_l),
-(44, road_t_s_b_r),
-(45, road_t_s_t_l),
-(46, road_t_s_t_r),
-(47, road_v),
-(48, t_junction_d),
-(49, t_junction_l),
-(50, t_junction_r),
-(51, t_junction_u))
+(0, dirt_1,"dirt_1"),
+(1, dirt_2,"dirt_2"),
+(2, grass_1,"grass_1"),
+(3, grass_2,"grass_2"),
+(4, c_junction,"cross_junction"),
+(5, dirt_b_s_b_l,"dirt_border_square_bottom_left"),
+(6, dirt_b_s_b_r,"dirt_border_square_bottom_right"),
+(7, dirt_b_c_d,"dirt_border_circle_down"),
+(8, dirt_b_c_b_l,"dirt_border_circle_bottom_left"),
+(9, dirt_b_c_b_r,"dirt_border_circle_bottom_right"),
+(10, dirt_b_c_l,"dirt_border_circle_left"),
+(11, dirt_b_c_r,"dirt_border_circle_right"),
+(12, dirt_b_c_u,"dirt_border_circle_up"),
+(13, dirt_b_c_t_l,"dirt_border_circle_top_left"),
+(14, dirt_b_c_t_r,"dirt_border_circle_top_right"),
+(15, dirt_b_s_d,"dirt_border_square_down"),
+(16, dirt_b_s_l,"dirt_border_square_left"),
+(17, dirt_b_s_r,"dirt_border_square_right"),
+(18, dirt_b_s_u,"dirt_border_square_up"),
+(19, dirt_b_s_t_l,"dirt_border_square_top_left"),
+(20, dirt_b_s_t_r,"dirt_border_square_top_right"),
+(21, grass_b_s_b_l,"grass_border_square_bottom_left"),
+(22, grass_b_s_b_r,"grass_border_square_bottom_right"),
+(23, grass_b_s_t_l,"grass_border_square_top_left"),
+(24, grass_b_s_t_r,"grass_border_square_top_right"),
+(25, grass_b,"grass_box"),
+(26, l_junction_b_l,"l_junction_bottom_left"),
+(27, l_junction_b_r,"l_junction_bottom_right"),
+(28, l_junction_t_l,"l_junction_top_left"),
+(29, l_junction_t_r,"l_junction_top_right"),
+(30, road_e_d,"road_end_down"),
+(31, road_e_l,"road_end_left"),
+(32, road_e_r,"road_end_right"),
+(33, road_e_u,"road_end_up"),
+(34, road_h,"road_horizontal"),
+(35, road_t_dirt_d,"road_to_dirt_down"),
+(36, road_t_dirt_l,"road_to_dirt_left"),
+(37, road_t_dirt_r,"road_to_dirt_right"),
+(38, road_t_dirt_u,"road_to_dirt_up"),
+(39, road_t_c_b_l,"road_turn_circle_bottom_left"),
+(40, road_t_c_b_r,"road_turn_circle_bottom_right"),
+(41, road_t_c_t_l,"road_turn_circle_top_left"),
+(42, road_t_c_t_r,"road_turn_circle_top_right"),
+(43, road_t_s_b_l,"road_turn_square_bottom_left"),
+(44, road_t_s_b_r,"road_turn_square_bottom_right"),
+(45, road_t_s_t_l,"road_turn_square_top_left"),
+(46, road_t_s_t_r,"road_turn_square_top_right"),
+(47, road_v,"road_vertical"),
+(48, t_junction_d,"t_junction_down"),
+(49, t_junction_l,"t_junction_left"),
+(50, t_junction_r,"t_junction_right"),
+(51, t_junction_u,"t_junction_up"))
 
 
 block_grid = []
@@ -130,8 +130,7 @@ for x in range(25):
         block_grid[x].append(Block((x*30,y*30)))
 
 display = pygame.display.set_mode((800,800))
-pygame.display.set_caption("theo died in the making of this")
-
+pygame.display.set_caption("Level Editor")
 mouse_index = 1
 print("1")
 display.fill((255,255,255))
@@ -172,6 +171,6 @@ while True:
     
     with open("level_save","w") as f:
         json.dump(data,f)
-
+    pygame.display.set_caption(images[mouse_index][2])
     pygame.display.update()
     clock.tick(60)
